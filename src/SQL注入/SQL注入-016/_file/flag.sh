ad17190fa9c66dc28f0528aedefa76b4ff3b12b0
wjlin0 <wjlgeren@163.com>
2023-11-29 13:10:45 +0800

    2023-11-29

--- src/SQL注入/SQL注入-016/_file/flag.sh
+++ src/SQL注入/SQL注入-016/_file/flag.sh
@@ -2,25 +2,13 @@
 sleep 1
 
 
-
-flagfile=/var/www/html/db.sql
-
-if [ -f $flagfile ]; then
-    # 从环境变量中获取 flag
-    custom_flag="flag{not_here}"
-    if [ -n "$custom_flag" ]; then
-        sed -i "s/flag{\*\*\**}/$custom_flag/" $flagfile
-    fi
-fi
-
-
 default_flag="flag{********************************}"
 custom_flag="${FLAG:-$default_flag}"
 
 echo -n "$custom_flag" > /flag
 chmod 400 /flag
 chown root:root /cat_flag
-chmod u+x /cat_flag
+chmod 4755 /cat_flag
 
 
 
