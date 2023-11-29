56028bbaf5b4911f82cd8062e9a9423be9d11245
wjlin0 <wjlgeren@163.com>
2023-11-29 17:47:00 +0800

    2023-11-29

--- src/SQL注入/SQL注入-015/_file/flag.sh
+++ src/SQL注入/SQL注入-015/_file/flag.sh
@@ -5,20 +5,11 @@ sleep 1
 # 修改 secure_file_priv 允许读写
 
 
-flagfile=/var/www/html/db.sql
-
-if [ -f $flagfile ]; then
-    # 从环境变量中获取 flag
-    custom_flag="flag{not_here}"
-    if [ -n "$custom_flag" ]; then
-        sed -i "s/flag{\*\*\**}/$custom_flag/" $flagfile
-    fi
-fi
 
 
 default_flag="flag{********************************}"
 custom_flag="${FLAG:-$default_flag}"
-
+echo "$custom_flag" > "/flag"
 
 export FLAG=not_here
 rm -f /flag.sh
